d8c610c24eb6e3c7dcea227cfdd12dac3a1737e2
cinta <naurah_zanjabila_33rpl@student.smktelkom-mlg.sch.id>
2025-11-13 11:31:43 +0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('nilai_siswa.csv')
+print(data.info())
+print(data.head())
+print(data.describe())
+
+print("Median:", data['Nilai'].median())
+print("Modus:", data['Nilai'].mode()[0])
+
+matematika = data[data['Mapel'] == 'Matematika']
+print(matematika)
+bahasaIndonesia = data[data['Mapel'] == 'Bahasa Indonesia']
+print(bahasaIndonesia)
+bahasaInggris = data[data['Mapel'] == 'Bahasa Inggris']
+print(bahasaInggris)
+produktif = data[data['Mapel'] == 'Produktif']
+print(produktif)
+
+data.groupby('Mapel')['Nilai'].agg(['max','min'])
+
+rata = data.groupby('Mapel')['Nilai'].mean()
+rata.plot(kind='bar')
+plt.title('Rata-Rata Nilai per Mapel')
+plt.xlabel('Mata Pelajaran')
+plt.ylabel('Nilai Rata-Rata')
+plt.show()
+
+sns.boxplot(x='Mapel', y='Nilai', data=data)
+plt.title('Sebaran Nilai per Mata Pelajaran')
+plt.show()
